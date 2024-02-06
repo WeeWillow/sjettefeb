b4629beb4d4a73cbdde47e2cbe036d8b1431dd1a
WeeWillow <weecalamy@gmail.com>
2024-02-06 13:39:00 +0100

    lesson

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,37 @@
-let myAge = 26;
-let myName = "Monica";
-let myShoeSize = 38.5;
+// const theParagraphElement = document.querySelector(".osty");
 
-console.log("Hejsa: ", myName)
+// let myName = "Monica";
+// console.error('myName:', myName);
+
+// let userInput = prompt("Hvor gammel er du?");
+
+// theParagraphElement.textContent = myName;
+
+// let image = "<img src=\"dog.png\"/>";
+// console.log('image:', image)
+
+// let myname = "Monica";
+// let myage = 26;
+// console.log(myname + " og jeg er " + myage);
+
+// let mysentance = "skriv noget her eller et eller andet";
+// console.log('mysentance:', mysentance.replace("noget", "ost"));
+
+// const number = Math.sqrt(65)
+// console.log(number)
+
+// const numberround = Math.round(number)
+// console.log(numberround)
+
+let numberOfShoes = prompt("Hvor mange sko?");
+
+if (numberOfShoes > 30) {
+    console.log("You have a problem! Sell some of them.");
+}
+else if (numberOfShoes < 20) {
+    console.log("You need to slow down.");
+}
+
+else if (numberOfShoes > 10) {
+    console.log("Youv've got it under control, for now...");
+}
